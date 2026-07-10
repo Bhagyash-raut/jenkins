68411758dec4aeaabb0245016aca764bd8058783
Bhagyash-raut <bhagyashraut9@gmail.com>
2026-07-10 11:24:14 +0530

    Add Terraform variables for project configuration
    Define variables for project configuration including node settings.

---
+++ variable.tf
@@ -0,0 +1,24 @@
+variable "project" {
+  type    = string
+  default = "cbz"
+}
+
+variable "desired_nodes" {
+  type    = number
+  default = 2
+}
+
+variable "min_nodes" {
+  type    = number
+  default = 1
+}
+
+variable "max_nodes" {
+  type    = number
+  default = 3
+}
+
+variable "node_instance_type" {
+  type    = string
+  default = "c7i-flex.large"
+}
