df33955e2633b4cafcea0f098dedcd4fd5dc3528
Bhagyash-raut <bhagyashraut9@gmail.com>
2026-07-10 11:22:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,72 +2,70 @@ provider "aws" {
   region = "ap-south-2"
 }
 
-# -----------------------------
-# EKS Cluster IAM Role
-# -----------------------------
+# Create an IAM Role for the EKS Cluster
 resource "aws_iam_role" "eks_cluster_role" {
   name = "eks-cluster-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = "sts:AssumeRole"
-
-      Principal = {
-        Service = "eks.amazonaws.com"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
       }
-    }]
+    ]
   })
 }
 
+# Attach necessary policies to the EKS Cluster IAM Role
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "eks_vpc_resource_controller" {
+resource "aws_iam_role_policy_attachment" "eks_service_policy" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
 }
 
-# -----------------------------
-# Node Group IAM Role
-# -----------------------------
+# Create an IAM Role for the Node Group
 resource "aws_iam_role" "eks_node_role" {
   name = "eks-node-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = "sts:AssumeRole"
-
-      Principal = {
-        Service = "ec2.amazonaws.com"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
       }
-    }]
+    ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "worker_node_policy" {
+# Attach necessary policies to the Node Group IAM Role
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "cni_policy" {
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "ecr_readonly" {
+resource "aws_iam_role_policy_attachment" "ec2_container_policy" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-# -----------------------------
-# Default VPC
-# -----------------------------
+# Fetch the Default VPC and Subnets
 data "aws_vpc" "default" {
   default = true
 }
@@ -79,9 +77,7 @@ data "aws_subnets" "default" {
   }
 }
 
-# -----------------------------
-# EKS Cluster
-# -----------------------------
+# Create an EKS Cluster
 resource "aws_eks_cluster" "cbz_cluster" {
   name     = "${var.project}-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
@@ -92,13 +88,11 @@ resource "aws_eks_cluster" "cbz_cluster" {
 
   depends_on = [
     aws_iam_role_policy_attachment.eks_cluster_policy,
-    aws_iam_role_policy_attachment.eks_vpc_resource_controller
+    aws_iam_role_policy_attachment.eks_service_policy
   ]
 }
 
-# -----------------------------
-# Node Group
-# -----------------------------
+# Create a Node Group
 resource "aws_eks_node_group" "cbz_nodegroup" {
   cluster_name    = aws_eks_cluster.cbz_cluster.name
   node_group_name = "${var.project}-node-group"
@@ -114,8 +108,9 @@ resource "aws_eks_node_group" "cbz_nodegroup" {
   instance_types = [var.node_instance_type]
 
   depends_on = [
-    aws_iam_role_policy_attachment.worker_node_policy,
-    aws_iam_role_policy_attachment.cni_policy,
-    aws_iam_role_policy_attachment.ecr_readonly
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.ec2_container_policy
   ]
 }
+
