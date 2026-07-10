d5e8e3c65dfb65a7b2c8da13f12e1eeb821ce298
Bhagyash-raut <bhagyashraut9@gmail.com>
2026-07-10 10:56:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -120,31 +120,6 @@ resource "aws_eks_node_group" "cbz_nodegroup" {
   ]
 }
 
-variable "project" {
-  type    = string
-  default = "cbz"
-}
-
-variable "desired_nodes" {
-  type    = number
-  default = 2
-}
-
-variable "min_nodes" {
-  type    = number
-  default = 1
-}
-
-variable "max_nodes" {
-  type    = number
-  default = 3
-}
-
-variable "node_instance_type" {
-  type    = string
-  default = "c7i-flex.large"
-}
-
 project            = "cbz"
 desired_nodes      = 2
 min_nodes          = 1
